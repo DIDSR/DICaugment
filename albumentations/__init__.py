863df6e1fe188df3629c88b4ca5738361f552c69
jjmcintosh <jjmcintosh@github.com>
2023-06-20 13:26:04 -0400

    remove old name

--- albumentations/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-__version__ = "1.0"
-
-from .augmentations import *
-from .core.composition import *
-from .core.serialization import *
-from .core.transforms_interface import *
